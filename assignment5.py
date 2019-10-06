38f410cda55e419b35b7287eb8cbb3e1addc2dcc
Wisha-khan <khanwisha005@gmail.com>
2019-10-06 14:10:09 +0500

    My first commit

---
+++ assignment5.py
@@ -0,0 +1,10 @@
+>>>print ("Hello World")
+>>>print ("You must be legendary God that comes from the sky")
+>>>print ("We have been waiting for you for a long time")
+>>>x=6
+>>>print (x)
+>>>y=x*7
+>>>print (y)
+$cat hello.py
+print ( "hello world")
+$python hello.py 
