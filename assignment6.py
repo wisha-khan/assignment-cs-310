9fed4f53970c4af1cff4cefb63896b7a067f491a
wisha-khan <khanwisha005@gmail.com>
2019-10-10 14:45:50 +0500

    My first commit

---
+++ assignment6.py
@@ -0,0 +1,25 @@
+>>>print (1,000,000)
+>>> message = "And now for something completely different"
+>>> n= 17
+>>> pi = 3.1415926535897931
+>>>print (n)
+>>>print (pi)
+>>>print (1)
+>>> x = 2
+>>>print (x)
+>>> minute = 59
+>>> minute//60
+>>> 1+1
+>>> quotient = 7 // 3
+>>>print (quotient)
+>>> remainder = 7 % 3
+>>>print (remainder)
+>>> first = 10
+>>> second = 15
+>>> print (first+second)
+>>> first = "test"
+>>> second = 3
+print (first*second)
+>>> name = input ( "what is your name?/n")
+>>> print (name)
+
